72b892d7697e17cd6fb45dc425041df4082b341a
lzh11 <2239040999@qq.com>
2015-11-11 21:17:55 +0800

    Initial Commit

---
+++ 01/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  01
+//
+//  Created by LZH on 15/11/11.
+//  Copyright © 2015年 LZH. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
